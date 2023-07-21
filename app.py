49b86852a697427787f15ebe46a04cd19da5ce34
nathalierocelle <70849363+nathalierocelle@users.noreply.github.com>
2023-07-21 16:28:26 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
 # ---- READ DATA ----
-data = './data/supermarkt_sales.csv'
+data = 'supermarkt_sales.csv'
 df = pd.read_csv(data)
 
 # ---- MODIFY THE DATA ---- 
